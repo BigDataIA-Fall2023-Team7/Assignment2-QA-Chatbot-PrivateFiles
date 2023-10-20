e627d3dc9adef0dad482fcff7f77f718bc62a324
Aditya Kawale <52578828+aditya26091999@users.noreply.github.com>
2023-10-20 13:01:42 -0400

    Update architecture-assgn1.py

--- frontend/pages/architecture-assgn1.py
+++ frontend/pages/architecture-assgn1.py
@@ -5,6 +5,7 @@ from PIL import Image
 #To set the page configurations
 st.set_page_config(page_title="Asn2", page_icon='❷', layout="wide", initial_sidebar_state="auto", menu_items=None)
 
+st.write(os.getcwd())
 image = Image.open(os.getcwd() + '/images/pdf_processing_flow.png')
 
 st.markdown("<h1 style='text-align: center;'>Architecture Diagram (Assignment 1)</h1>", unsafe_allow_html=True)
