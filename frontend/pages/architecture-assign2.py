c179481cf884bd9b848f277dfbfa5f31bad3e79c
Aditya Kawale <52578828+aditya26091999@users.noreply.github.com>
2023-10-20 13:04:15 -0400

    Update architecture-assign2.py

--- frontend/pages/architecture-assign2.py
+++ frontend/pages/architecture-assign2.py
@@ -5,7 +5,7 @@ from PIL import Image
 #To set the page configurations
 st.set_page_config(page_title="Asn2", page_icon='❷', layout="wide", initial_sidebar_state="auto", menu_items=None)
 
-image = Image.open(os.getcwd() + '/images/qa_chatbot_for_pdfs_architecture.png')
+image = Image.open(os.getcwd() + '/frontend/images/qa_chatbot_for_pdfs_architecture.png')
 
 st.markdown("<h1 style='text-align: center;'>Architecture Diagram (Assignment 2)</h1>", unsafe_allow_html=True)
 st.image(image, use_column_width=True)
